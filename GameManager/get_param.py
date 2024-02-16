082925b3a88b4fb1580f94ea87aa9974a7c1618b
Cameron Showalter <cjshowalter@alaska.edu>
2024-02-15 15:26:59 -0900

    Split apart stack, to help with rapid development soon (won't wait as long for redeploys). Also update get_param to be more dynamic

---
+++ GameManager/get_param.py
@@ -0,0 +1,51 @@
+
+import os
+
+from typing import Union
+
+from aws_cdk import (
+    Stack,
+    CfnParameter,
+)
+
+
+def get_param(stack: Stack, key: str, default: Union[str, int, float]=None, description: str="") -> str:
+    """
+    Get a parameter from the environment, or use the default if it's not set.
+
+        Stack: The stack to add the parameter to
+        key: The name of the parameter to grab from env vars
+        default: The default value if not set. DON'T set this if you want it to be required.
+        description: The description to attach to the param in AWS
+    """
+    # Figure out what the value should be
+    val = os.environ.get(key) or default
+    if val is None:
+        raise ValueError(f"Missing required parameter: '{key}', and no default is set.")
+    # val could be a string of int, because of os.environ.get anyways,
+    # and CfnParameter only accepts strings. Keep it as a string here
+    val = str(val)
+
+    # Let param_type be dynamic, we're not in CFN Yaml anymore.
+    try:
+        float(val)
+        param_type = "Number"
+    except ValueError:
+        param_type = "String"
+
+    # This is to see what the stack was deployed with in the cfn parameters tab:
+    CfnParameter(
+        stack,
+        key,
+        type=param_type,
+        default=val,
+        # Since changing it in the console won't do anything, don't let them:
+        allowed_values=[val],
+        description=f"{description}{' ' if description else ''}(Don't Edit in CloudFormation Console)"
+    )
+
+    # If you're expecting a number, change it away from a string:
+    #   (CfnParameter *wants* the input as a string, but nothing else does)
+    if param_type == "Number":
+        val = float(val) if "." in val else int(val)
+    return val
---
+++ GameManager/vpc_base_stack.py
@@ -0,0 +1,43 @@
+
+
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    CfnParameter,
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns,
+    aws_iam as iam,
+    aws_autoscaling as autoscaling,
+)
+
+
+class VpcBaseStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        print(f"Stack: {Stack.of(self).account}")
+
+        # Create a Public VPC to run instances in:
+        self.vpc = ec2.Vpc(
+            self,
+            "Vpc",
+            nat_gateways=0,
+            max_azs=2,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name=f"public-{construct_id}",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                )
+            ]
+        )
+
+        ## ECS / EC2 Security Group (Same since using bridge I think?):
+        # https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/SecurityGroup.html
+        self.sg_ecs_traffic = ec2.SecurityGroup(self, "sg-ecs-traffic", vpc=self.vpc, allow_all_outbound=False)
+        self.sg_ecs_traffic.connections.allow_to(
+            ec2.Peer.any_ipv4(),
+            ec2.Port.tcp(443),
+            description="Allow HTTPS traffic OUT. Let ECS talk with EC2 to register instances",
+        )
+
