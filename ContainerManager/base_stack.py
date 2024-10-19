ef437aa14e17809badf98a0f2c8355878291b434
Cameron Showalter <cjshowalter@alaska.edu>
2024-10-19 12:41:07 -0800

    Got the base_stack able to deploy by itself when leafs are attached, by fixing export_value error

--- ContainerManager/base_stack.py
+++ ContainerManager/base_stack.py
@@ -130,6 +130,17 @@ class ContainerManagerBaseStack(Stack):
             )
             self.root_hosted_zone.apply_removal_policy(RemovalPolicy.DESTROY)
 
+        #####################
+        ### Export Values ###
+        #####################
+        ## To stop cdk from trying to delete the exports when cdk is deployed by
+        ## itself, but has leaf stacks attached to it.
+        # https://blogs.thedevs.co/aws-cdk-export-cannot-be-deleted-as-it-is-in-use-by-stack-5c205b8004b4
+        self.export_value(self.vpc.vpc_id)
+        self.export_value(self.ssh_key_pair.key_pair_name)
+        self.export_value(self.sns_notify_topic.topic_arn)
+        for subnet in self.vpc.public_subnets:
+            self.export_value(subnet.subnet_id)
 
         #####################
         ### cdk_nag stuff ###
